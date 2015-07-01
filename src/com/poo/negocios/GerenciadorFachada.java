839fd27c1661e80b681d0277d295a225466e096a
raylison100 <Raylison@10.3.2.48>
2015-07-01 09:12:54 -0300

    teste

--- src/com/poo/negocios/GerenciadorFachada.java
+++ src/com/poo/negocios/GerenciadorFachada.java
@@ -3,15 +3,22 @@ package com.poo.negocios;
 
 import java.io.IOException;
 
+import com.poo.execoes.CadatroAutoExistenteExeception;
+import com.poo.execoes.CadatroContraExistenteExeception;
 import com.poo.execoes.CadatroPessoaExistenteExeception;
+import com.poo.execoes.ProcuraAutoInexistenteExeception;
 import com.poo.execoes.ProcuraPessoaInexistenteExeception;
 import com.poo.execoes.SenhaIncorretaExeception;
+import com.poo.negocios.beans.Automovel;
 import com.poo.negocios.beans.Pessoa;
 
 
 public class GerenciadorFachada implements IGerenciador{
 
 	private CadastroPessoa pessoa = new CadastroPessoa();
+	private CadastroAuto auto = new CadastroAuto();
+	private CadastroContrato contra = new CadastroContrato();
+	private CadastroSinistro sini = new CadastroSinistro();
 	private ValidarSenha vS = new ValidarSenha();
 	private static IGerenciador instance;
 	
@@ -70,6 +77,35 @@ public class GerenciadorFachada implements IGerenciador{
 		confirmada = this.vS.checarSenhaS(nome, senha);
 		
 		return confirmada;
+	}
+	
+	
+public void cadatrarAuto(Automovel a) throws IOException, CadatroAutoExistenteExeception, ProcuraAutoInexistenteExeception{
+		
+		this.auto.cadatrar(a);
+		
+	}
+		
+	public void exibiAuto() throws IOException{
+		
+		this.auto.
+	}
+	
+    public void exibiFuncionario() throws IOException{
+ 		
+		this.pessoa.exibiFuncionarios();
+	}
+		
+	public void excluirPessoa(String nome) throws IOException, ProcuraPessoaInexistenteExeception{
+		
+		this.pessoa.descadatrar(nome);
+	}
+		
+	public Pessoa pesquisarPessoa(String nome) throws ProcuraPessoaInexistenteExeception{
+		
+		Pessoa c = this.pessoa.acharPessoa(nome);
+		return c;
+		
 	}
 		
 }
